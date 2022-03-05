0fb7a3714b449f1695d6c17433e484dff7de6a5c
ketanyadav8807 <ketanyadavbdh8807@gmail.com>
2022-03-05 17:01:26 +0530

    Completed WITH Responsiv

--- meesho/src/LSPL/Signup.jsx
+++ meesho/src/LSPL/Signup.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField';
 import style from "./signup.module.css"
-
+import {Clear} from '@material-ui/icons';
+import {IconButton} from "@mui/material"
+import { useNavigate } from "react-router-dom"
 export const Signup = () => {
+
+    const navigate = useNavigate()
+
     const [phoneNum, setPhoneNum] = useState("");
 
     const sendPhoneNum = (phoneNum) =>{
@@ -24,7 +29,10 @@ export const Signup = () => {
 
   return (
     <div className={style.body}>
-        
+        <div className={style.title}>
+            <IconButton onClick={() => navigate("/home")}><Clear style={{fontSize: 26 , color: "#666362"}} /></IconButton>
+            <img src={require("../images/meeshoLogo.png")} alt="" style={{width: 100,height: 30, marginLeft: "3px", marginTop: "5px" }}/>
+        </div>
         <div className={style.mainSignupBox}>
             <div className={style.img}><img src='https://meesho.com/_next/static/images/authTopBanner-6792b3e68f63d623b8ba99556d38d56d.jpg' className={style.img}/></div>
             <div className={style.upperPart}>
@@ -33,15 +41,15 @@ export const Signup = () => {
 
                 <div className={style.PhoneNumDiv}>
                     <h4 className={style.h4IN}><span className={style.IN}>IN</span>+91</h4>
-                    <TextField id="standard-basic" label="Phone Number" onChange={(e)=> setPhoneNum(e.currentTarget.value)}/>
+                    <TextField id="standard-basic" label="Phone Number" onChange={(e)=> setPhoneNum(e.currentTarget.value)} className={style.tests}/>
                 </div>
 
                 <div><button className={style.SendOTP} onClick={()=> sendPhoneNum(phoneNum)}>Send OTP</button></div>
             </div>
         
             <div className={style.lowerPart}>
-                <div>By continuing, you agree to Meesho’s</div>
-                <div><span className={style.textColor}>Terms & Conditions</span> and <span className={style.textColor}>Privacy Policy</span></div>
+                <div className={style.lowerPartLow}>By continuing, you agree to Meesho’s</div>
+                <div className={style.lowerPartLoww}><span className={style.textColor}>Terms & Conditions</span> and <span className={style.textColor}>Privacy Policy</span></div>
             </div>
         </div>
     </div>
