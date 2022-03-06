d17a772516968a376a35db8a119f44b91d2d1bcd
Shubham168 <jadhavshubham168@gmail.com>
2022-03-06 19:24:54 +0530

    fixed the error

--- meesho/src/Components/productDetails/ProductDetail.jsx
+++ meesho/src/Components/productDetails/ProductDetail.jsx
@@ -12,15 +12,6 @@ import {
 import { CartIcon } from "../../Icon/CartIcon";
 import { Loading } from "../../Components/Loading";
 import { CartContext } from "../../Contexts/CartProvider";
- 
-import { AuthContext } from "../../Contexts/AuthProvider";
-import { LocalOfferOutlined, AssignmentReturnOutlined,AttachMoneyOutlined } from "@material-ui/icons";
-
-export const ProductDetail = ({ refItem, fetchURL }) => {
-
-  
-  const navigate = useNavigate()
-  const [isLoading, setIsLoading] = useState(false)
 
 import {
   LocalOfferOutlined,
@@ -28,7 +19,7 @@ import {
   AttachMoneyOutlined,
 } from "@material-ui/icons";
 
-export const ProductDetail = ({ refItem, fetchURL }) => {
+export const ProductDetail = ({ fetchURL }) => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
   const [isDataLoading, setIsDataLoading] = useState(false);
@@ -38,11 +29,11 @@ export const ProductDetail = ({ refItem, fetchURL }) => {
   let { id } = useParams();
 
   let authToken = JSON.parse(localStorage.getItem("userToken"));
-  let isLoggedIn = false 
-  if(authToken){
-     isLoggedIn = true
+  let isLoggedIn = false;
+  if (authToken) {
+    isLoggedIn = true;
   }
-  console.log(isLoggedIn)
+  console.log(isLoggedIn);
   useEffect(() => {
     setIsDataLoading(true);
     fetch(`${fetchURL}/${id}`)
@@ -53,7 +44,10 @@ export const ProductDetail = ({ refItem, fetchURL }) => {
         setSelected(d.images[0]);
         setIsDataLoading(false);
       })
-      .catch((err) => {console.log(err);setIsDataLoading(false);});
+      .catch((err) => {
+        console.log(err);
+        setIsDataLoading(false);
+      });
   }, []);
 
   const changeSelectedImg = (imgUrl) => {
@@ -75,8 +69,7 @@ export const ProductDetail = ({ refItem, fetchURL }) => {
     productDetailsArray.push([i, productDetails[i]]);
   }
 
-
-  const {getCount} = useContext(CartContext)
+  const { getCount } = useContext(CartContext);
   const handleCart = () => {
     setIsLoading(true);
     try {
@@ -99,17 +92,17 @@ export const ProductDetail = ({ refItem, fetchURL }) => {
           setIsLoading(false);
           getCount();
 
-          if(isLoggedIn){
-          navigate("/checkout/cart")
-          }else{
-            navigate("/auth/signup")
+          if (isLoggedIn) {
+            navigate("/checkout/cart");
+          } else {
+            navigate("/auth/signup");
           }
-      })
-  } catch (error) {
-      console.log(error)
-      setIsLoading(false)
-  }
-  }
+        });
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
 
   console.log(selected);
   if (isLoading) return <Loading />;
@@ -126,14 +119,23 @@ export const ProductDetail = ({ refItem, fetchURL }) => {
             order: { xs: 2, sm: 2, md: 2, lg: 1 },
           }}
         >
-          {!isDataLoading ? (img &&
+          {!isDataLoading ? (
+            img &&
             img.map((imgUrl) => (
               <img
                 src={imgUrl}
                 width="45px"
                 onClick={(e) => changeSelectedImg(e.currentTarget.src)}
               />
-            ))):(<Skeleton animation="wave" variant="rectangular" width={45} height={118}  />)}
+            ))
+          ) : (
+            <Skeleton
+              animation="wave"
+              variant="rectangular"
+              width={45}
+              height={118}
+            />
+          )}
         </Grid>
         <Grid
           sx={{
@@ -156,16 +158,25 @@ export const ProductDetail = ({ refItem, fetchURL }) => {
               boxSizing: "border-box",
             }}
           >
-            {!isDataLoading ? (<img
-              src={selected}
-              style={{
-                objectFit: "contain",
-                minWidth: "100%",
-                maxWidth: "100%",
-                minHeight: "100%",
-                maxHeight: "100%",
-              }}
-            />):(<Skeleton animation="wave" variant="rectangular" width={358} height={600}  />)}
+            {!isDataLoading ? (
+              <img
+                src={selected}
+                style={{
+                  objectFit: "contain",
+                  minWidth: "100%",
+                  maxWidth: "100%",
+                  minHeight: "100%",
+                  maxHeight: "100%",
+                }}
+              />
+            ) : (
+              <Skeleton
+                animation="wave"
+                variant="rectangular"
+                width={358}
+                height={600}
+              />
+            )}
           </Grid>
           <Grid item py={2} borderBottom="1px solid rgba(0, 0, 0, 0.2)">
             <Button
@@ -199,157 +210,166 @@ export const ProductDetail = ({ refItem, fetchURL }) => {
               boxShadow: "none",
             }}
           >
-            {!isDataLoading ?(<CardContent sx={{ fontSize: "25px" }}>
-              <Typography
-                component="div"
-                color="gray"
-                sx={{
-                  whiteSpace: "nowrap",
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  fontSize: "20px",
-                  marginBottom: "1%",
-                }}
-              >
-                {data.title}
-              </Typography>
-              <Grid
-                sx={{
-                  display: "flex",
-                  alignContent: "flex-start",
-                  alignItems: "center",
-                  marginBottom: "1%",
-                }}
-              >
+            {!isDataLoading ? (
+              <CardContent sx={{ fontSize: "25px" }}>
                 <Typography
-                  variant="h4"
                   component="div"
-                  align="left"
-                  color="black"
-                  sx={{ marginRight: "15px", fontFamily: "Mier-Bold" }}
-                >
-                  Rs.{data.discounted_price}
-                </Typography>
-                <Typography
                   color="gray"
-                  align="left"
-                  component="p"
-                  fontSize={18}
-                  sx={{ marginRight: "15px", textDecoration: "line-through" }}
-                >
-                  Rs.{data.original_price}
-                </Typography>
-                <Typography
-                  color="green"
-                  align="left"
-                  component="p"
-                  sx={{ fontSize: "20px", marginBottom: "1%" }}
-                >
-                  {100 -
-                    Math.floor(
-                      (data.discounted_price / data.original_price) * 100
-                    )}
-                  % off
-                </Typography>
-              </Grid>
-              <Grid
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  paddingRight: "3px",
-                  marginBottom: "2%",
-                }}
-              >
-                <svg
-                  viewBox="0 0 16 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="16"
-                  width="16"
-                  color="greenT2"
-                  iconSize="20"
-                  style={{ paddingRight: "3px" }}
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M13.833 5.733c0 .186 1.006 1.745 1.006 1.745.215.312-.844 2.27-.844 2.27-.105.151-.168 2.029-.168 2.029-.001.385-1.961 1.317-1.961 1.317-.17.058-1.276 1.54-1.276 1.54-.218.31-2.328-.139-2.328-.139-.17-.057-1.898.462-1.898.462-.352.118-1.806-1.541-1.806-1.541-.105-.151-1.796-.793-1.796-.793-.35-.12-.595-2.356-.595-2.356 0-.186-1.005-1.744-1.005-1.744-.215-.312.844-2.271.844-2.271.104-.151.167-2.029.167-2.029.002-.385 1.962-1.317 1.962-1.317.17-.058 1.275-1.54 1.275-1.54.218-.31 2.089.353 2.089.353.17.058 2.138-.676 2.138-.676l1.805 1.541c.106.151 1.797.793 1.797.793.35.12.594 2.356.594 2.356zM6.761 6.761a.875.875 0 11-1.238-1.237.875.875 0 011.238 1.237zm2.865-1.236a.6.6 0 11.848.849l-4.101 4.1a.6.6 0 11-.849-.848l4.102-4.1zm.846 4.95a.875.875 0 11-1.238-1.238.875.875 0 011.238 1.237z"
-                    fill="#06A759"
-                  ></path>
-                </svg>
-                <Typography fontSize={"18px"}>
-                  Rs.100 off on 1st order
-                </Typography>
-              </Grid>
-              {data.discountedPrice > 300 ? (
-                <Typography
-                  align="left"
-                  component="p"
                   sx={{
-                    padding: "1px 2px",
-                    backgroundColor: "rgb(249, 249, 249)",
-                    width: "fit-content",
+                    whiteSpace: "nowrap",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
                     fontSize: "20px",
-                    borderRadius: "5px",
                     marginBottom: "1%",
                   }}
                 >
-                  free Delivery
+                  {data.title}
                 </Typography>
-              ) : (
-                <Typography
-                  align="left"
-                  component="p"
+                <Grid
                   sx={{
-                    padding: "1px 2px",
-                    backgroundColor: "rgb(249, 249, 249)",
-                    width: "fit-content",
+                    display: "flex",
+                    alignContent: "flex-start",
+                    alignItems: "center",
+                    marginBottom: "1%",
+                  }}
+                >
+                  <Typography
+                    variant="h4"
+                    component="div"
+                    align="left"
+                    color="black"
+                    sx={{ marginRight: "15px", fontFamily: "Mier-Bold" }}
+                  >
+                    Rs.{data.discounted_price}
+                  </Typography>
+                  <Typography
+                    color="gray"
+                    align="left"
+                    component="p"
+                    fontSize={18}
+                    sx={{ marginRight: "15px", textDecoration: "line-through" }}
+                  >
+                    Rs.{data.original_price}
+                  </Typography>
+                  <Typography
+                    color="green"
+                    align="left"
+                    component="p"
+                    sx={{ fontSize: "20px", marginBottom: "1%" }}
+                  >
+                    {100 -
+                      Math.floor(
+                        (data.discounted_price / data.original_price) * 100
+                      )}
+                    % off
+                  </Typography>
+                </Grid>
+                <Grid
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    paddingRight: "3px",
+                    marginBottom: "2%",
+                  }}
+                >
+                  <svg
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="16"
+                    width="16"
+                    color="greenT2"
+                    iconSize="20"
+                    style={{ paddingRight: "3px" }}
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M13.833 5.733c0 .186 1.006 1.745 1.006 1.745.215.312-.844 2.27-.844 2.27-.105.151-.168 2.029-.168 2.029-.001.385-1.961 1.317-1.961 1.317-.17.058-1.276 1.54-1.276 1.54-.218.31-2.328-.139-2.328-.139-.17-.057-1.898.462-1.898.462-.352.118-1.806-1.541-1.806-1.541-.105-.151-1.796-.793-1.796-.793-.35-.12-.595-2.356-.595-2.356 0-.186-1.005-1.744-1.005-1.744-.215-.312.844-2.271.844-2.271.104-.151.167-2.029.167-2.029.002-.385 1.962-1.317 1.962-1.317.17-.058 1.275-1.54 1.275-1.54.218-.31 2.089.353 2.089.353.17.058 2.138-.676 2.138-.676l1.805 1.541c.106.151 1.797.793 1.797.793.35.12.594 2.356.594 2.356zM6.761 6.761a.875.875 0 11-1.238-1.237.875.875 0 011.238 1.237zm2.865-1.236a.6.6 0 11.848.849l-4.101 4.1a.6.6 0 11-.849-.848l4.102-4.1zm.846 4.95a.875.875 0 11-1.238-1.238.875.875 0 011.238 1.237z"
+                      fill="#06A759"
+                    ></path>
+                  </svg>
+                  <Typography fontSize={"18px"}>
+                    Rs.100 off on 1st order
+                  </Typography>
+                </Grid>
+                {data.discountedPrice > 300 ? (
+                  <Typography
+                    align="left"
+                    component="p"
+                    sx={{
+                      padding: "1px 2px",
+                      backgroundColor: "rgb(249, 249, 249)",
+                      width: "fit-content",
+                      fontSize: "20px",
+                      borderRadius: "5px",
+                      marginBottom: "1%",
+                    }}
+                  >
+                    free Delivery
+                  </Typography>
+                ) : (
+                  <Typography
+                    align="left"
+                    component="p"
+                    sx={{
+                      padding: "1px 2px",
+                      backgroundColor: "rgb(249, 249, 249)",
+                      width: "fit-content",
+                      fontSize: "14px",
+                      borderRadius: "5px",
+                      marginBottom: "1%",
+                    }}
+                  >
+                    Delivery Rs.70
+                  </Typography>
+                )}
+
+                <Grid
+                  sx={{
+                    display: "flex",
+                    alignContent: "flex-start",
                     fontSize: "14px",
-                    borderRadius: "5px",
+                    alignItems: "center",
                     marginBottom: "1%",
                   }}
                 >
-                  Delivery Rs.70
-                </Typography>
-              )}
+                  <Typography
+                    color="white"
+                    align="left"
+                    component="p"
+                    sx={{
+                      padding: "5px 10px",
+                      width: "fit-content",
+                      fontSize: "18px",
+                      borderRadius: "40px",
+                      border: "1px solid rgb(249, 249, 249)",
+                      marginTop: "5px",
+                      marginRight: "8px",
 
-              <Grid
-                sx={{
-                  display: "flex",
-                  alignContent: "flex-start",
-                  fontSize: "14px",
-                  alignItems: "center",
-                  marginBottom: "1%",
-                }}
-              >
-                <Typography
-                  color="white"
-                  align="left"
-                  component="p"
-                  sx={{
-                    padding: "5px 10px",
-                    width: "fit-content",
-                    fontSize: "18px",
-                    borderRadius: "40px",
-                    border: "1px solid rgb(249, 249, 249)",
-                    marginTop: "5px",
-                    marginRight: "8px",
-
-                    backgroundColor: ratingBgColor,
-                  }}
-                >
-                  {data.rating} &#9733;
-                </Typography>
-                <Typography
-                  align="left"
-                  component="p"
-                  fontSize={18}
-                  color={"gray"}
-                >
-                  Reviews
-                </Typography>
-              </Grid>
-            </CardContent>):(<Skeleton animation="wave" variant="rectangular" width={708} height={600}  />)}
+                      backgroundColor: ratingBgColor,
+                    }}
+                  >
+                    {data.rating} &#9733;
+                  </Typography>
+                  <Typography
+                    align="left"
+                    component="p"
+                    fontSize={18}
+                    color={"gray"}
+                  >
+                    Reviews
+                  </Typography>
+                </Grid>
+              </CardContent>
+            ) : (
+              <Skeleton
+                animation="wave"
+                variant="rectangular"
+                width={708}
+                height={600}
+              />
+            )}
           </Card>
 
           <Card
